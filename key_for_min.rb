3f095ebc0f499706b1bd18482a818f36b0f8f0b0
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-10 01:38:50 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,39 @@
 
 def key_for_min_value(name_hash)
 
+arr = []
+  
+ name_hash.each do |key, value|
+ 
+  arr << value
+
+end
+ 
+
+i = 1
+j = 0
+while i < arr.length 
+
+
+  if arr[j] < arr[i]
+  
+  i+=1
+
+  else arr[j] > arr[i]
+
+  arr[j] = arr[i]
+  i+=1 
+  end
+
+
+end  
+
+lowest = arr[j] 
+
+name_hash.each do |key, value|
+
+ return key if arr[j] == value 
+
+end
+ 
 end
